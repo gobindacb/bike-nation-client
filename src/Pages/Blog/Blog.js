ece7c97ac8858436f067510d88c62df61aa348bc
gobindacb <p.gobinda.cb@gmail.com>
2022-11-30 23:30:49 +0600

    blog added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <p>Welcome to Bike Blog</p>
-        </div>
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+            <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+                <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
+                <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+                <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
+                    <div>
+                        <h3 className="font-semibold">What are the different ways to manage a state in a React application?</h3>
+                        <p className="mt-1 dark:text-gray-400">As your application grows, it helps to be more intentional about how your state is organized and how the data flows between your components. Redundant or duplicate state is a common source of bugs. In this chapter, you’ll learn how to structure your state well, how to keep your state update logic maintainable, and how to share state between distant components.</p>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold">How does prototypical inheritance work?</h3>
+                        <p className="mt-1 dark:text-gray-400">The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object</p>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+                        <p className="mt-1 dark:text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+                        <p className="mt-1 dark:text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
