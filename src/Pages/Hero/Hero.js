2459c552606b54191bfd3a3ba842d06aecfefe5d
gobindacb <p.gobinda.cb@gmail.com>
2022-11-30 23:40:36 +0600

    hero page edited

--- src/Pages/Hero/Hero.js
+++ src/Pages/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
                 <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-white-900">
                     <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl"><span className='text-secondary'>Bike</span> <span className='text-violet-600'>Nation</span></h1>
                     <h1 className="text-2xl font-bold leading-none sm:text-3xl xl:max-w-3xl text-white-100">Dream On Wheels</h1>
-                    <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-white">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-white">You can buy or sell your used bike here.</p>
                     <div className="flex flex-wrap justify-center">
                         <button type="button" className="px-8 py-3 m-2 text-lg font-semibold rounded bg-gray-800 text-gray-50">Get started</button>
                         <button type="button" className="px-8 py-3 m-2 text-lg border rounded border-gray-700 text-white">Learn more</button>
