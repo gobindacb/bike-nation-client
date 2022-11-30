80a8fb15f0438c94b60c3e55c6ef105e4bf7f390
gobindacb <p.gobinda.cb@gmail.com>
2022-11-30 19:07:36 +0600

    advertisement section added

---
+++ src/Pages/Advertisement/Advertisement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Advertisement = () => {
+    return (
+        <div>
+            <p>Advertisement</p>
+        </div>
+    );
+};
+
+export default Advertisement;
